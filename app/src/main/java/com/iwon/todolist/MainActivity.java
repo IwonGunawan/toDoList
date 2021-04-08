f141d9bf47a1654cf01ef71c167b240e4f868eea
Iwon <iwongunawan@gmail.com>
2021-04-08 18:58:08 +0700

    save local pt 2

--- app/src/main/java/com/iwon/todolist/MainActivity.java
+++ app/src/main/java/com/iwon/todolist/MainActivity.java
@@ -1,18 +1,37 @@
 package com.iwon.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.textfield.TextInputEditText;
+import com.iwon.todolist.helper.DbHelper;
 import com.iwon.todolist.utils.*;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "track_todo";
 
     TextInputEditText tvNote;
     LinearLayout lLDate;
+    MaterialCalendarView mcv;
+    Button btnSubmit;
+    String sNote;
+    String sDate;
+
+    DbHelper dbHelper = new DbHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +57,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     void showDatePicker(){
-        CalendarDialog dialog = new CalendarDialog(this);
+        BottomSheetDialog dialog = new BottomSheetDialog(this);
+        dialog.setContentView(R.layout.dialog_calendar);
         dialog.show();
 
+        mcv = dialog.findViewById(R.id.calendar_view);
+        btnSubmit = dialog.findViewById(R.id.btn_submit);
+        String[] today = getToday();
 
+        mcv.setSelectionMode(MaterialCalendarView.SELECTION_MODE_SINGLE);
+        mcv.state().edit()
+                .setMinimumDate(CalendarDay.from(2011, 7, 16))
+                .setMaximumDate(CalendarDay.from(Integer.parseInt(today[0]), Integer.parseInt(today[1]) -1, Integer.parseInt(today[2])))
+                .commit();
+        mcv.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                sDate = (date.getMonth() +1 ) + "-" + date.getDay() + "-" + date.getYear();
+            }
+        });
 
+        btnSubmit.setOnClickListener(view -> {
+            submit();
+            dialog.dismiss();
+        });
     }
+
+    private void submit(){
+        sNote = tvNote.getText().toString();
+
+        if (!sNote.equals(null) || !sDate.equals(null)){
+            // save data
+            dbHelper.save(sNote, sDate);
+            clear();
+        } else {
+            Toast.makeText(this, "Please fill NOTE and DATE", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String[] getToday() {
+        String[] result = new String[3];
+
+        Date today = new Date();
+        result[0] = (String) DateFormat.format("yyyy", today);
+        result[1] = (String) DateFormat.format("MM", today);
+        result[2] = (String) DateFormat.format("dd", today);
+
+        return result;
+    }
+
+    private void clear(){
+        tvNote.setText(null);
+        sNote = null;
+        sDate = null;
+    }
+
+
 
 }
--- app/src/main/java/com/iwon/todolist/helper/DbHelper.java
+++ app/src/main/java/com/iwon/todolist/helper/DbHelper.java
@@ -1,4 +1,100 @@
 package com.iwon.todolist.helper;
 
-class DbHelper {
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper {
+    private static final String TAG = DbHelper.class.getSimpleName();
+
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "todo.db";
+    private static final String TB_TODO = "todo_list";
+
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_DONE = "done";
+    private static final String COLUMN_NOTE = "note";
+    private static final String COLUMN_DATE = "date";
+
+    private static final int DONE_FALSE = 0;
+    private static final int DONE_TRUE = 1;
+
+
+    public DbHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String createTbTodo = "CREATE TABLE " + TB_TODO + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_DONE + " CHART(1) NOT NULL, " +
+                COLUMN_NOTE + " VARCHAR NOT NULL, " +
+                COLUMN_DATE + " VARCHAR NOT NULL" +
+                ")";
+        Log.d(TAG, "onCreate: " + createTbTodo);
+        sqLiteDatabase.execSQL(createTbTodo);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public ArrayList<HashMap<String, String>> allData(){
+        ArrayList<HashMap<String, String>> wordList = new ArrayList<HashMap<String, String>>();
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TB_TODO;
+        Log.d(TAG, "allData: " + query);
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()){
+            do {
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put(COLUMN_ID, cursor.getString(0));
+                map.put(COLUMN_DONE, cursor.getString(1));
+                map.put(COLUMN_NOTE, cursor.getString(2));
+                map.put(COLUMN_DATE, cursor.getString(3));
+                wordList.add(map);
+            } while (cursor.moveToNext());
+        }
+        db.close();
+
+        return wordList;
+    }
+
+
+    public void save(String note, String date){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "INSERT INTO " + TB_TODO + " (" + COLUMN_DONE + ", " + COLUMN_NOTE + "," + COLUMN_DATE + ") " +
+                "VALUES ('" + DONE_FALSE +"', '"+ note +"', '"+ date +"')";
+        Log.d(TAG, "save: " + query);
+        db.execSQL(query);
+        db.close();
+    }
+
+    public void update(int id, int done){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "UPDATE " + TB_TODO + " SET " + COLUMN_DONE + "='" + done + "' WHERE " + COLUMN_ID + "=" + id;
+        Log.d(TAG, "update: " + query);
+        db.execSQL(query);
+        db.close();
+    }
+
+    public void delete(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "DELETE FROM " + TB_TODO + " WHERE " + COLUMN_ID + "=" + id;
+        Log.d(TAG, "delete: " + query);
+        db.execSQL(query);
+        db.close();
+    }
+
 }
